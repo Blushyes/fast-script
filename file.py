b2638ae78a903c0974b19651b7efd03bcbeb0e4f
Blushyes <Blushyes@gmail.com>
2023-12-22 14:21:05 +0800

    ✨ 新增写入文本到文件函数

--- file.py
+++ file.py
@@ -12,10 +12,12 @@ class FileInfo:
 
 
 def loads_file(path: str, safely=False) -> str:
-    """读取某个文件的内容
+    """读取某个文件的文本内容，utf-8编码
 
     Args:
-        path (str): 文件的路径
+        path: 文件的路径
+        safely: 如果为 true 则遇见异常的时候不会报错
+
 
     Returns:
         str: 文件所有的内容，UTF-8编码，如果文件不存在或者出现其他异常，返回一个空串
@@ -34,12 +36,14 @@ def list_files_recursive(
     directory: str = os.path.curdir,
     exclude: list[str] | str = None,
     file_filter: Callable[[FileInfo], list[FileInfo]] = None,
+    safely=False,
 ) -> list[FileInfo]:
     """递归列出所有的文件，默认为当前目录下面所有文件
 
     Args:
         exclude: 排除哪个或者哪些文件，只是简单地排除某个文件名或者目录名。
         file_filter: 过滤器，可以更自由地定义排除哪些文件。
+        safely: 如果为 true 则遇见异常的时候不会报错
 
     Returns:
         收集到的所有文件信息
@@ -68,11 +72,21 @@ def list_files_recursive(
 
             file_info = FileInfo(
                 name=filename,
-                content=loads_file(relative_path),
+                content=loads_file(relative_path, safely),
                 abspath=full_path,
                 relpath=relative_path,
             )
 
             files_list.append(file_info)
 
-    return list(filter(file_filter, files_list)) if files_list else files_list
+    return list(filter(file_filter, files_list)) if file_filter else files_list
+
+
+def write_text(text: str, file_path: str, safely=False) -> None:
+    """以utf-8编码写入文本到某个文件"""
+    try:
+        with open(file_path, "w", encoding="utf8") as f:
+            f.write(text)
+    except Exception as e:
+        if not safely:
+            raise e
