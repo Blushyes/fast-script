370e9e469e24510815b7065be8404a2fec3a6654
Blushyes <Blushyes@gmail.com>
2023-12-24 15:30:57 +0800

    👷 采用GitHub的Tag作为版本号

--- setup.py
+++ setup.py
@@ -1,12 +1,11 @@
 from setuptools import setup, find_packages
 
-# 读取README.md的内容作为long_description
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setup(
     name="fast_script_utils",
-    version="0.0.1",
+    version=__version__,
     packages=find_packages(),
     long_description=long_description,
     long_description_content_type="text/markdown",
