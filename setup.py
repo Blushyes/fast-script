bbe44e60623692774749a8256f03caeb06f553b0
Blushyes <75828899+Blushyes@users.noreply.github.com>
2023-12-24 14:34:19 +0800

    :construction_worker: 发布的时候自动上传到PyPI

--- setup.py
+++ setup.py
@@ -1,8 +1,14 @@
 from setuptools import setup, find_packages
 
+# 读取README.md的内容作为long_description
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
     name="fast_script_utils",
     version="0.0.1",
     packages=find_packages(),
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     install_requires=[],  # 依赖列表，格式是 ['package>=version']
 )
