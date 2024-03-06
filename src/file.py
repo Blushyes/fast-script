f49c5d373daec48d625558371d552738c86f2a64
Blushyes <Blushyes@gmail.com>
2024-03-06 14:08:58 +0800

    :sparkles: file新增链式调用

--- src/file.py
+++ src/file.py
@@ -1,6 +1,6 @@
 import os
 from dataclasses import dataclass
-from typing import Callable
+from typing import Callable, Collection
 
 
 @dataclass
@@ -33,16 +33,18 @@ def loads_file(path: str, safely=False) -> str:
 
 
 def list_files_recursive(
-    directory: str = os.path.curdir,
-    exclude: list[str] | str = None,
-    file_filter: Callable[[FileInfo], list[FileInfo]] = None,
-    safely=True,
+        directory: str = os.path.curdir,
+        exclude: Collection[str] | str = None,
+        file_filter: Callable[[FileInfo], bool] | Collection[
+            Callable[[FileInfo], bool]] = None,
+        safely=True,
 ) -> list[FileInfo]:
     """递归列出所有的文件，默认为当前目录下面所有文件
 
     Args:
+        directory: 目录
         exclude: 排除哪个或者哪些文件，只是简单地排除某个文件名或者目录名
-        file_filter: 过滤器，可以更自由地定义排除哪些文件
+        file_filter: 过滤器，可以更自由地定义排除哪些文件，可以是列表也可以是单个
         safely: 如果为 true 则遇见异常的时候不会报错
 
     Returns:
@@ -54,6 +56,9 @@ def list_files_recursive(
     if isinstance(exclude, str):
         exclude = [exclude]
 
+    if isinstance(file_filter, Callable):
+        file_filter = [file_filter]
+
     # 使用os.walk遍历目录
     for root, _, files in os.walk(directory):
         for file in files:
@@ -63,7 +68,7 @@ def list_files_recursive(
             full_path = os.path.join(root, file)
 
             if exclude and any(
-                filename == name or name in full_path for name in exclude
+                    filename == name or name in full_path for name in exclude
             ):
                 continue
 
@@ -79,7 +84,13 @@ def list_files_recursive(
 
             files_list.append(file_info)
 
-    return list(filter(file_filter, files_list)) if file_filter else files_list
+    filtered_list = None
+    if file_filter:
+        filtered_list = files_list
+        for e in file_filter:
+            filtered_list = filter(e, filtered_list)
+
+    return list(filtered_list) if filtered_list else files_list
 
 
 def write_text(text: str, file_path: str, safely=False) -> None:
@@ -90,3 +101,36 @@ def write_text(text: str, file_path: str, safely=False) -> None:
     except Exception as e:
         if not safely:
             raise e
+
+
+class FileProcessChain:
+    def __init__(self):
+        self._path: str | None = None
+        self._safely: bool = False
+        self._exclude_files: list[str] = []
+        self._filters: list[Callable[[FileInfo], bool]] = []
+
+    def path(self, path: str) -> 'FileProcessChain':
+        self._path = path
+        return self
+
+    def safely(self) -> 'FileProcessChain':
+        self._safely = True
+        return self
+
+    def exclude(self, *name: str) -> 'FileProcessChain':
+        self._exclude_files.extend(name)
+        return self
+
+    def filter(self, filter_func: Callable[[FileInfo], bool]) -> 'FileProcessChain':
+        self._filters.append(filter_func)
+        return self
+
+    def collect(self) -> list[FileInfo]:
+        if self._path is None:
+            raise Exception("path is required")
+
+        return list_files_recursive(self._path, self._exclude_files, self._filters, self._safely)
+
+    def safe_collect(self) -> list[FileInfo]:
+        return self.safely().collect()
